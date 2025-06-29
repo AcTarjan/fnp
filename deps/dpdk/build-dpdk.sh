f0b382f325ed2b90cd589b24ecabe427e4f76596
gyk0933 <gyk0933@qq.com>
2025-06-29 16:44:36 +0800

    feat(all): 删除picotls和dpdk的库文件

--- deps/dpdk/build-dpdk.sh
+++ deps/dpdk/build-dpdk.sh
@@ -2,10 +2,11 @@ CURRENT_DIR=$(pwd)
 echo "current dir: $CURRENT_DIR"
 echo "start build dpdk..."
 
-INSTALL_DIR=${dirname $CURRENT_DIR}
+#INSTALL_DIR=${dirname $CURRENT_DIR}
 
+tar -xf dpdk-22.11.8.tar.xz
 cd ./dpdk-stable-22.11.8
-meson setup --prefix=INSTALL_DIR build
+meson setup --prefix=$CURRENT_DIR build
 
 cd ./build
 ninja -j8
